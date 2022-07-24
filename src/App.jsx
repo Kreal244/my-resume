bc215104cb4ba5165b3b7e0567da55277ff57837
Huynh Nguyen Khang <76195700+Krealvippro@users.noreply.github.com>
2022-07-24 15:54:38 +0700

    add  music

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.scss';
 import Header from './Component/Header';
 import Body from './Component/Body';
+import React from 'react';
+import ReactAudioPlayer from 'react-audio-player';
+import music from './ImageSource/musicBackground.mp3'
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body/>
+      <Body />
+      <ReactAudioPlayer
+  src={music}
+  autoPlay
+/>
     </div>
   );
 }
